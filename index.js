65916ec62bb9e60d1064bef527bd8f02f7d6c181
nai golf <niy_golf@hotmail.com>
2021-07-10 02:56:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const cluster = require("cluster");
 const net = require("net");
 const RL = require("readline");
 
-let user = "",
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'), function() {
+        console.log('running on port', app.get('port'))
+    })
+
+app.get('/', function (req, res) {
+        res.end('duinocoin-golf')
+    })
+
+
+let user = "naigolf",
     processes = 1;
 
 console.clear();
